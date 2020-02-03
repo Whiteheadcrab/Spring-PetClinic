5437a14171883f9dfbc3ad66fca60b8727b6bfb1
Whiteheadcrab <lettersorstudy@gmail.com>
2020-02-03 19:35:23 +0100

    Version 1.1.3 Updating pom files and providing clean + package process

--- PetClinic/petclinicWeb/src/main/java/springpetclinic/SpringPetclinicApplication.java
+++ PetClinic/petclinicWeb/src/main/java/springpetclinic/SpringPetclinicApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringPetclinicApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringPetclinicApplication.class, args);
     }
 
