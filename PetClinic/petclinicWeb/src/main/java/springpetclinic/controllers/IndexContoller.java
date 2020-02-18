dc1be1368612c6589e26732b317ae6ec8331b243
Whiteheadcrab <lettersorstudy@gmail.com>
2020-02-18 13:59:47 +0100

    Version 1.8.2 Implemented simple variant of Index Controller

---
+++ PetClinic/petclinicWeb/src/main/java/springpetclinic/controllers/IndexContoller.java
@@ -0,0 +1,14 @@
+package springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexContoller
+{
+    @RequestMapping({"","/","index","index.html"})
+    public String Index()
+    {
+        return "index";
+    }
+}
