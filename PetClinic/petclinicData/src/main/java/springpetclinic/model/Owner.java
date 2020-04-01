e660bc84b9fe39f40f84b2f82836f9002bda0177
Whiteheadcrab <lettersorstudy@gmail.com>
2020-04-01 07:28:29 +0200

    Version 1.18.1 Imported Vet class inside Owner

--- PetClinic/petclinicData/src/main/java/springpetclinic/model/Owner.java
+++ PetClinic/petclinicData/src/main/java/springpetclinic/model/Owner.java
@@ -1,6 +1,19 @@
 package springpetclinic.model;
 
 
+import java.util.Set;
+
 public class Owner extends Person
 {
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets()
+    {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets)
+    {
+        this.pets = pets;
+    }
 }
