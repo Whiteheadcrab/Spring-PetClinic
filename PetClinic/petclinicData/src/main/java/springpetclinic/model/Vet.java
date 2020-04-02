3f69ca26ac24dafca88c9189d5408da39a6226d5
Whiteheadcrab <lettersorstudy@gmail.com>
2020-04-02 09:29:01 +0200

    Version 1.19.2 Imported Speciality Entity in Vet class . close #26

--- PetClinic/petclinicData/src/main/java/springpetclinic/model/Vet.java
+++ PetClinic/petclinicData/src/main/java/springpetclinic/model/Vet.java
@@ -1,6 +1,18 @@
 package springpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person
 {
+    private Set<Speciality> specialities;
 
+    public Set<Speciality> getSpecialities()
+    {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities)
+    {
+        this.specialities = specialities;
+    }
 }
