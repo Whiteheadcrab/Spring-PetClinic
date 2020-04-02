7d11a788a38420981df3166443e54202eb491300
Whiteheadcrab <lettersorstudy@gmail.com>
2020-04-02 09:25:24 +0200

    Version 1.19.1 Implemented Speciality class

---
+++ PetClinic/petclinicData/src/main/java/springpetclinic/model/Speciality.java
@@ -0,0 +1,16 @@
+package springpetclinic.model;
+
+public class Speciality extends BaseEntity
+{
+    private String description;
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+}
