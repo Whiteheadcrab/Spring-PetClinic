e2298cc939120f4738c67170df13d1ede16b7b42
Whiteheadcrab <lettersorstudy@gmail.com>
2020-04-04 10:31:24 +0200

    Version 1.21.1 Implemented PetType Map Service . close #27

---
+++ PetClinic/petclinicData/src/main/java/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package springpetclinic.services;
+
+import springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>
+{
+
+}
---
+++ PetClinic/petclinicData/src/main/java/springpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,42 @@
+package springpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import springpetclinic.model.Pet;
+import springpetclinic.model.PetType;
+import springpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends  AbstactMapService<PetType, Long> implements PetTypeService
+{
+    @Override
+    public Set<PetType> findAll()
+    {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id)
+    {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object)
+    {
+        return super.save(object.getId() , object);
+    }
+
+    @Override
+    public void delete(PetType object)
+    {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id)
+    {
+        super.deleteById(id);
+    }
+}
