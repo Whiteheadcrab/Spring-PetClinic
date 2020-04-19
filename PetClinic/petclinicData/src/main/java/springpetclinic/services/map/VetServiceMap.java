de055b5077fdb36ceb1f54cd25b2b6daa40f77a6
Whiteheadcrab <lettersorstudy@gmail.com>
2020-04-19 20:51:33 +0200

    petVersion 1.25.3 Modifying VetService Map for proper saving of Speciality to Vets

--- PetClinic/petclinicData/src/main/java/springpetclinic/services/map/VetServiceMap.java
+++ PetClinic/petclinicData/src/main/java/springpetclinic/services/map/VetServiceMap.java
@@ -1,12 +1,21 @@
 package springpetclinic.services.map;
 
+import springpetclinic.model.Speciality;
 import springpetclinic.model.Vet;
 import springpetclinic.services.CrudService;
+import springpetclinic.services.SpecialityService;
 
 import java.util.Set;
 
 public class VetServiceMap extends AbstactMapService<Vet , Long> implements CrudService<Vet, Long>
 {
+    private final SpecialityService specialityService;
+
+    public VetServiceMap(SpecialityService specialityService)
+    {
+        this.specialityService = specialityService;
+    }
+
     @Override
     public Set<Vet> findAll()
     {
@@ -22,6 +31,18 @@ public class VetServiceMap extends AbstactMapService<Vet , Long> implements Crud
     @Override
     public Vet save(Vet object)
     {
+        if(object.getSpecialities().size() > 0 )
+        {
+            object.getSpecialities().forEach(
+                    speciality ->
+                    {
+                        if(speciality.getId() == null)
+                        {
+                            Speciality saveSpeciality = specialityService.save(speciality);
+                            speciality.setId(saveSpeciality.getId());
+                        }
+                    });
+        }
         return super.save(object.getId() , object);
     }
 
