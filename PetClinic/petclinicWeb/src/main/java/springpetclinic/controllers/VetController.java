2a5b915a3b0ceff221e3de8d97947543741bcbbe
Whiteheadcrab <lettersorstudy@gmail.com>
2020-04-21 19:08:39 +0200

    petVersion 1.26.4 Modify VetController for proper work

--- PetClinic/petclinicWeb/src/main/java/springpetclinic/controllers/VetController.java
+++ PetClinic/petclinicWeb/src/main/java/springpetclinic/controllers/VetController.java
@@ -1,14 +1,24 @@
 package springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springpetclinic.services.VetService;
 
 @Controller
 public class VetController
 {
-    @RequestMapping({"/vets" , "/vets/index" , "/vets/index.html"})
-    public String ListVets()
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets" , "/vets/index" , "/vets/index.html","/vets.html"})
+    public String listVets(Model model)
     {
+        model.addAttribute("vets",vetService.findAll());
+
         return  "vets/index";
     }
 }
