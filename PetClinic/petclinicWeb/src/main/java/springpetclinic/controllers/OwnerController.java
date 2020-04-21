d6c33d2c66dd02c1bf53ea105f9dadc747729794
Whiteheadcrab <lettersorstudy@gmail.com>
2020-04-21 19:01:36 +0200

    petVersion 1.26.2 Modify OwnerController for future using Not Implemented template

--- PetClinic/petclinicWeb/src/main/java/springpetclinic/controllers/OwnerController.java
+++ PetClinic/petclinicWeb/src/main/java/springpetclinic/controllers/OwnerController.java
@@ -1,15 +1,32 @@
 package springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springpetclinic.services.OwnerService;
+
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController
 {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    public  String listOwners()
+    public String listOwners(Model model)
     {
+        model.addAttribute("owners",ownerService.findAll());
+
         return "owners/index";
+    }
+
+    @RequestMapping("/find")
+    public String findOwner()
+    {
+        return "notimplemented";
     }
 }
