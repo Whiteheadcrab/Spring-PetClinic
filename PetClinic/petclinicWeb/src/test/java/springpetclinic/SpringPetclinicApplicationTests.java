b332f06c92572dc5888ec22a2c2eadb1ffb427ec
Whiteheadcrab <lettersorstudy@gmail.com>
2020-06-23 12:04:20 +0200

    2.02.1 Upgraded from JUnit 4 into JUnit 5 . close #61

--- PetClinic/petclinicWeb/src/test/java/springpetclinic/SpringPetclinicApplicationTests.java
+++ PetClinic/petclinicWeb/src/test/java/springpetclinic/SpringPetclinicApplicationTests.java
@@ -1,13 +1,17 @@
 package springpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SpringPetclinicApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads()
+    {
     }
 
 }
