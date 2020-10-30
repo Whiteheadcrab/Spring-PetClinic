60702f0c9cda8e624987ca039b4c2a1a73698235
Whiteheadcrab <lettersorstudy@gmail.com>
2020-10-30 16:45:06 +0100

    Version 2.09.1 Solved issue with unworking process . close #65

--- PetClinic/petclinicWeb/src/main/java/springpetclinic/controllers/OwnerController.java
+++ PetClinic/petclinicWeb/src/main/java/springpetclinic/controllers/OwnerController.java
@@ -57,7 +57,7 @@ public class OwnerController
         }
 
         // find owners by last name
-        List<Owner> results = ownerService.findAllByLastNameLike("%"+ owner.getLastName() + "%");
+        List<Owner> results = ownerService.findAllByLastNameLike("%" + owner.getLastName() + "%");
 
         if (results.isEmpty())
             {
