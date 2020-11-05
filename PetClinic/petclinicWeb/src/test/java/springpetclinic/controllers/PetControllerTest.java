429b5591ed20f084e7a8185fc9b13991760f06b8
Whiteheadcrab <lettersorstudy@gmail.com>
2020-11-05 17:15:07 +0100

    Version 2.11.1 Implemented Test for not implemented PetController for starting implementation by TDD methodology .

---
+++ PetClinic/petclinicWeb/src/test/java/springpetclinic/controllers/PetControllerTest.java
@@ -0,0 +1,114 @@
+package springpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import springpetclinic.model.Owner;
+import springpetclinic.model.PetType;
+import springpetclinic.services.OwnerService;
+import springpetclinic.services.PetService;
+import springpetclinic.services.PetTypeService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+public class PetControllerTest
+{
+    @Mock
+    PetService petService;
+
+    @Mock
+    OwnerService ownerService;
+
+    @Mock
+    PetTypeService petTypeService;
+
+    @InjectMocks
+    PetController petController;
+
+    MockMvc mockMvc;
+
+    Owner owner;
+    Set<PetType> petTypes;
+
+    @BeforeEach
+    public void setUp()
+    {
+        owner = Owner.builder().id(1l).build();
+
+        petTypes = new HashSet<>();
+        petTypes.add(PetType.builder().id(1L).name("Dog").build());
+        petTypes.add(PetType.builder().id(2L).name("Cat").build());
+
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(petController)
+                .build();
+    }
+
+    @Test
+    public void initCreationForm() throws Exception
+    {
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypes);
+
+        mockMvc.perform(get("/owners/1/pets/new"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(model().attributeExists("pet"))
+                .andExpect(view().name("pets/createOrUpdatePetForm"));
+    }
+
+    @Test
+    public void processCreationForm() throws Exception
+    {
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypes);
+
+        mockMvc.perform(post("/owners/1/pets/new"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"));
+
+        verify(petService).save(any());
+    }
+
+    @Test
+    public void initUpdateForm() throws Exception
+    {
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypes);
+        when(petService.findById(anyLong())).thenReturn(Pet.builder().id(2L).build());
+
+        mockMvc.perform(get("/owners/1/pets/2/edit"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(model().attributeExists("pet"))
+                .andExpect(view().name("pets/createOrUpdatePetForm"));
+    }
+
+    @Test
+    public void processUpdateForm() throws Exception
+    {
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypes);
+
+        mockMvc.perform(post("/owners/1/pets/2/edit"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"));
+
+        verify(petService).save(any());
+    }
+
+}
