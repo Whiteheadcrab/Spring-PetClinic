15ae371b050855740715ebab569e241c76b0e344
Whiteheadcrab <lettersorstudy@gmail.com>
2020-11-06 14:32:46 +0100

    Version 2.11.4 Added builder pattern to PetType .  #57

--- PetClinic/petclinicData/src/main/java/springpetclinic/model/PetType.java
+++ PetClinic/petclinicData/src/main/java/springpetclinic/model/PetType.java
@@ -11,11 +11,17 @@ import javax.persistence.Table;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity
 {
+    @Builder
+    public PetType(Long id, String name)
+    {
+        super(id);
+        this.name = name;
+    }
+
     @Column(name = "name")
     private String name;
 
