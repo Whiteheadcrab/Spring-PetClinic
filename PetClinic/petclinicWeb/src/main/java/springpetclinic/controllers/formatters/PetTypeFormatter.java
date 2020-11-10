94cb18576f10aa13761cea06ab7c8f1c64affa84
Whiteheadcrab <lettersorstudy@gmail.com>
2020-11-10 14:29:31 +0100

    Version 2.12.1 Implementing Formatter for PetType solved appeared problem . close #66

---
+++ PetClinic/petclinicWeb/src/main/java/springpetclinic/controllers/formatters/PetTypeFormatter.java
@@ -0,0 +1,43 @@
+package springpetclinic.controllers.formatters;
+
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+import springpetclinic.model.PetType;
+import springpetclinic.services.PetTypeService;
+
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Locale;
+
+@Component
+public class PetTypeFormatter implements Formatter<PetType>
+{
+    private final PetTypeService petTypeService;
+
+    public PetTypeFormatter(PetTypeService petTypeService)
+    {
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public String print(PetType petType, Locale locale)
+    {
+        return petType.getName();
+    }
+
+    @Override
+    public PetType parse(String text, Locale locale) throws ParseException
+    {
+        Collection<PetType> findPetTypes = petTypeService.findAll();
+
+        for (PetType type : findPetTypes)
+        {
+            if (type.getName().equals(text))
+            {
+                return type;
+            }
+        }
+
+        throw new ParseException("type not found: " + text, 0);
+    }
+}
